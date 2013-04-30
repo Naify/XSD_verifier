f04a49e11fd8e726b6b6b2843bbfdfe561797ffd
Naydenov Yury <naifyk91@gmail.com>
2013-04-30 17:00:26 +0400

    first commit

---
+++ server/server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import os
+import sys
+import subprocess
+import csv
+import time
+import cgi
+import cgitb
+import lxml
+
+#cgitb.enable()
+# Извлечение данных о запросе
+#data = cgi.FieldStorage()
+
+# Извлечение полей запроса
+#action = data.getvalue("action") 
+
+
+
+sys.exit(0)
