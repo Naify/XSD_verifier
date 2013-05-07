f8b7f14e0d843de6cab9c0ddb49b7d53688d720c
Naydenov Yury <naifyk91@gmail.com>
2013-05-07 12:34:05 +0400

    added form object, some cosmetic changes

--- lib.js
+++ lib.js
@@ -1,6 +1,14 @@
 
-$(document).ready(function(){
-
-
-
-});
+function hideBtn(){
+	$('#res').html("File is loading");
+}
+	         
+function handleResponse(mes) {
+	$('#upload').show();
+	if (mes.errors != null) {
+		$('#res').html("Errors: " + mes.errors);
+	}   
+	else {
+		$('#res').html("File " + mes.name + " loaded");   
+	}   
+}
