b5b2b9cabd7030ef41df0c8fe107357fac605dfb
Naydenov Yury <naifyk91@gmail.com>
2013-05-08 15:41:51 +0400

    fixed html part, server part returns xml

--- server/server.py
+++ server/server.py
@@ -1,23 +1,42 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-
+#-*- coding: utf-8 -*-
 
 import os
 import sys
 import subprocess
-import csv
 import time
 import cgi
 import cgitb
 import lxml
 
-#cgitb.enable()
+cgitb.enable()
 # Извлечение данных о запросе
-#data = cgi.FieldStorage()
+form_data = cgi.FieldStorage()
+file_data = form_data.getvalue('xml')
 
 # Извлечение полей запроса
 #action = data.getvalue("action") 
 
+# def answer():
+	# fp =open('some/file','wb')
+	# fp.write(file_data)
+	# fp.close()
 
+#sys.stderr.write(file_data)
+
+print "Content-Type: text/xml"
+print
+print file_data
+
+# answer()
+
+# schema_root = etree.XML('''\
+# ...   <xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+# ...     <xsd:element name="a" type="xsd:integer"/>
+# ...   </xsd:schema>
+# ... ''')
+# schema = etree.XMLSchema(schema_root)
+# parser = etree.XMLParser(schema = schema)
+# root = etree.fromstring("<a>5</a>", parser)
 
 sys.exit(0)
