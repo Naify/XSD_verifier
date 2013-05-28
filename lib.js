ac637df4eceff10a5ec537ab0c461712d2befb97
Naydenov Yury <naifyk91@gmail.com>
2013-05-28 13:11:48 +0400

    'css changes'

--- lib.js
+++ lib.js
@@ -2,10 +2,20 @@ $(document).ready(function() {
 
 	$('#former').ajaxForm(function() {
 	
-		success: function(answer){		
-			$("#logbox").html(answer); //Insert chat log into the #chatbox div				
-		}
+		alert('123');
+		
+		// success: function(answer){		
+			// $("#logbox").html(answer); //Insert chat log into the #chatbox div				
+		//}
 		
 	});
 	
- });
+});
+
+// $(document).ready(function() { 
+
+	// $('#former').ajaxForm(function() { 
+		// alert("Thank you for your comment!"); 
+	// }); 
+	
+// });
